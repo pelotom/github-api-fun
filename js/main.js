137274ec1cb5ad38d650942e9f10544114476037
Tom Crockett <pelotom@gmail.com>
2013-12-20 01:01:33 -0800

    Use url templates, just for shits

--- js/main.js
+++ js/main.js
@@ -19,19 +19,25 @@ var ractive = new Ractive({
   }
 });
 
-$.get('https://api.github.com/repos/scala-ide/scala-ide/pulls/598').done(function (pr) {
-  ractive.set('pr', pr);
-  $.when(
-    $.get(pr._links.comments.href),
-    $.get(pr._links.review_comments.href)
-  ).done(function (
-    issueComments,
-    reviewComments
-  ) {
-    var comments = issueComments[0].concat(reviewComments[0]);
-    comments.sort(function (c1, c2) {
-      return new Date(c1.created_at).getTime() - new Date(c2.created_at).getTime();
-    })
-    ractive.set('comment_list', comments);
+$.get('https://api.github.com/').done(function (directory) {
+  var repoUrl = UriTemplate.parse(directory.repository_url).expand({ owner: 'scala-ide', repo: 'scala-ide' });
+  $.get(repoUrl).done(function (repo) {
+    var pullsUrl = UriTemplate.parse(repo.pulls_url).expand({ number: '598' });
+    $.get(pullsUrl).done(function (pr) {
+      ractive.set('pr', pr);
+      $.when(
+        $.get(pr._links.comments.href),
+        $.get(pr._links.review_comments.href)
+      ).done(function (
+        issueComments,
+        reviewComments
+      ) {
+        var comments = issueComments[0].concat(reviewComments[0]);
+        comments.sort(function (c1, c2) {
+          return new Date(c1.created_at).getTime() - new Date(c2.created_at).getTime();
+        })
+        ractive.set('comment_list', comments);
+      });
+    });
   });
 });
