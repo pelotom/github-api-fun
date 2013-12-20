5fa44ea043e3cff22a4c1a89a6625fe866e949ec
Tom Crockett <pelotom@gmail.com>
2013-12-20 00:25:20 -0800

    Meh

--- js/main.js
+++ js/main.js
@@ -21,9 +21,13 @@ var ractive = new Ractive({
 
 $.get('https://api.github.com/repos/scala-ide/scala-ide/pulls/598').done(function (pr) {
   ractive.set('pr', pr);
-  var fIssueComments = $.get(pr._links.comments.href);
-  var fReviewComments = $.get(pr._links.review_comments.href);
-  $.when(fIssueComments, fReviewComments).done(function (issueComments, reviewComments) {
+  $.when(
+    $.get(pr._links.comments.href),
+    $.get(pr._links.review_comments.href)
+  ).done(function (
+    issueComments,
+    reviewComments
+  ) {
     var comments = issueComments[0].concat(reviewComments[0]);
     comments.sort(function (c1, c2) {
       return new Date(c1.created_at).getTime() - new Date(c2.created_at).getTime();
